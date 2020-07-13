cb84f527c193c43537b9c5986ef9aecba58f86e0
Lauren Tsai <laurentsai@fb.com>
2020-07-13 08:21:22 -0700

    initial setup

---
+++ Sprout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sprout
+//
+//  Created by laurentsai on 7/13/20.
+//  Copyright © 2020 laurentsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sprout/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Sprout
+//
+//  Created by laurentsai on 7/13/20.
+//  Copyright © 2020 laurentsai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

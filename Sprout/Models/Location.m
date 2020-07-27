b0f033b9eef890ff9bb1a50189fdd06b952e637f
Lauren Tsai <laurentsai@fb.com>
2020-07-27 12:54:30 -0700

    comments on location

--- Sprout/Models/Location.m
+++ Sprout/Models/Location.m
@@ -9,7 +9,11 @@
 #import "Location.h"
 
 @implementation Location
-
+/**
+Creates and returns an Location  from a single dictionary
+@param[in] dictionary the dictionary to be converted
+@return the Location that was created
+*/
 + (instancetype)locationWithDictionary:(NSDictionary *)dictionary{
     Location *loc = [[Location alloc] init];
     loc.city=dictionary[@"city"];
@@ -19,7 +23,11 @@
     loc.streetAddress=dictionary[@"streetAddress1"];
     return loc;
 }
-
+/**
+Creates and returns an formatted address from a location object
+@param[in] loc the lcoation to be converted
+@return the address string that was created
+*/
 + (NSString *)addressFromLocation:(Location*)loc{
     return [NSString stringWithFormat:@"%@ \n%@, %@ %@", loc.streetAddress, loc.city, loc.state, loc.postalCode];
 }

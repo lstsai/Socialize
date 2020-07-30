ba62d2f02d37956a439faa11a52ef2445cdc87cd
Lauren Tsai <laurentsai@fb.com>
2020-07-30 09:37:59 -0700

    Set the map to view entire US when starting

--- Sprout/ViewControllers/MapViewController.m
+++ Sprout/ViewControllers/MapViewController.m
@@ -29,6 +29,7 @@ Instantiates the map view to show the given location with a marker
 -(void) setUpMap{
     GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:CAMERA_LAT longitude:CAMERA_LNG zoom:MAP_ZOOM/5];
     [self.mapView setMinZoom:MAP_ZOOM/5 maxZoom:MAP_ZOOM];
+    
     self.mapView.camera=camera;
     NSMutableArray* markers=[NSMutableArray new];
 
